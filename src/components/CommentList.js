7ccdfc5c5b3cba6647b3f431e462c490de3dbd18
bratyslav <beginner.altavista@gmail.com>
2019-07-18 09:47:40 +0300

    update comments

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import Comment from './Comment';
 
-const CommentList = ({ postId, comments }) => (
-  <div className="post__comments-list">
-    <div className="post__comments-list-title">
-      Comments:
-    </div>
-    {
-      comments
-        .filter((comment) => comment.postId === postId)
-        .map((comment) => <Comment comment={comment} />)
-    }
-  </div>
-);
+class CommentList extends React.Component {
+  state = {
+    isVisible: false
+  }
+
+  commentsShow = () => {
+    this.setState((prevState) => (
+      { isVisible: !prevState.isVisible }
+    ))
+  }
+
+  render() {
+    const { comments, postId } = this.props;
+    const { isVisible } = this.state;
+
+    return (
+      <div className="post__comments-list">
+        <div
+          className="post__comments-list-title"
+          onClick={this.commentsShow}
+        >
+          <h3>
+            Comments
+          </h3>
+        </div>
+        {
+          isVisible
+          ? comments
+              .filter((comment) => comment.postId === postId)
+              .map((comment) => <Comment comment={comment} />)
+          : ''    
+        }
+      </div>
+    )
+  }
+};
 
 export default CommentList;
